06f99d5dffacfe3425149108de3f3a4cbbcfe255
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-01-02 15:02:40 +0500

    Chapter -1 Inception

---
+++ App.js
@@ -0,0 +1,29 @@
+const heading1 = React.createElement(
+    "h1",
+    {
+        id: "title",
+    },
+    "heading 1"
+);
+
+const heading2 = React.createElement(
+    "h1",
+    {
+        id: "title",
+    },
+    "heading 2"
+);
+
+const container = React.createElement(
+    "div",
+    {
+        id: "container"
+    },
+    [heading1, heading2]
+)
+
+// console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(container)
