47ff9b58169a7fe4727942a77402a487c45deded
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-01-04 00:14:49 +0500

    chapter-3 JSX & functional component

--- App.js
+++ App.js
@@ -1,32 +1,30 @@
-import React from "react";
+
 import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement(
-  "h1",
-  {
-    id: "title",
-  },
-  "This line is coming from the parcel"
+//  JSX => React.createElement => object => HTML(DOM).
+
+// this is a normal javascript function.
+const Title = () => (           
+  <h1 id="title" key="1">
+      This is heading
+  </h1>
 );
 
-const heading2 = React.createElement(
-  "h1",
-  {
-    id: "title",
-  },
-  "heading 2"
-);
+// React Component
+//  - Functional component
+//    Name of the component starts with Capital letter, it is a good practice,not mandotory
 
-const container = React.createElement(
-  "div",
-  {
-    id: "container",
-  },
-  [heading1, heading2]
-);
+const HeaderComponent = () => {
+  return(
+    <div>
+         <Title />   {/* or it can be written as  { Title() }    */}
+      <h1>This is functional component</h1>
+      <h2>This is h2 tag</h2>
+    </div>
+  );
+}
 
-// console.log(heading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(container);
+root.render(<HeaderComponent/>);
