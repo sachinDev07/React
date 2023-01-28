99d84da833db807f18015324a3442af9368cd017
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-01-29 00:50:43 +0500

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,10 @@
 const Footer = () => {
-    return <h4>Footer of the website</h4>;
+    return (
+        <div className="footer">
+            <h3>Made with <span className="heart">{("< REACT />")}</span> by Sachin Kumar</h3>
+            <h5>&copy; 2023 <span className="footer_logo">foodCaffe'</span>. All rights reserved.</h5>
+        </div>
+    )
 };
 
 export default Footer;
