001ff992b2ef47b8e69da0ab95f2935431317674
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-01-31 14:37:37 +0500

    chapter-10

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import logo from ".././assests/images/logoImg.png";
 
 
 const Title = () => (
-    <Link to="/" className="title" id="title" >
+    <Link to="/" className="w-48" id="title" >
       <img className="logo" src={logo} alt="logo" />
     </Link>
 );
@@ -18,27 +18,27 @@ const Header = () => {
   // const isOnline = useOnline();
 
     return (
-      <div className="header">
+      <div className=" flex fixed left-0 top-0 w-full z-50 py-4 px-44 justify-between content-center bg-gray-100 shadow-lg">
         <Title /> 
         <div className="nav-items">
-          <ul>
-            <li><Link className="list-item" to="/" > Home </Link> </li>
-            <li><Link className="list-item" to="/about" > About </Link></li>
-            <li><Link className="list-item" to="/contact" > Contact </Link></li>
-            <li><Link className="list-item" to="/cart" > Cart </Link></li>
-            <li><Link className="list-item" to="/instamart">Instamart</Link></li>
+          <ul className="flex gap-4 text-xl  text-black ">
+            <li><Link className="p-2 hover:text-white hover:rounded-md hover:bg-blue-600 transition-all ease-in duration-300 m-auto text-center block" to="/" > Home </Link> </li>
+            <li><Link className="p-2 hover:text-white hover:rounded-md hover:bg-blue-600 transition-all ease-in duration-300 m-auto text-center block" to="/about" > About </Link></li>
+            <li><Link className="p-2 hover:text-white hover:rounded-md hover:bg-blue-600 transition-all ease-in duration-300 m-auto text-center block" to="/contact" > Contact </Link></li>
+            <li><Link className="p-2 hover:text-white hover:rounded-md hover:bg-blue-600 transition-all ease-in duration-300 m-auto text-center block" to="/cart" > Cart </Link></li>
+            <li><Link className="p-2 hover:text-white hover:rounded-md hover:bg-blue-600 transition-all ease-in duration-300 m-auto text-center block" to="/instamart">Instamart</Link></li>
             {/* <li>{isOnline ? "✅" : "🔴"}</li> */}
             <li>
                 {
                   isLoggedIn 
                   ? 
                     <button 
-                            className="btn"
+                            className="w-24 py-2 px-4  bg-blue-700 text-white border-none rounded-sm font-medium"
                             onClick={() => setIsLoggedIn(false)} > Logout 
                     </button> 
                   : 
                     <button 
-                            className="btn"
+                            className="py-2 px-4  bg-black text-white border-none rounded-sm font-medium"
                             onClick={() => setIsLoggedIn(true)} > Login 
                     </button>
                 }
