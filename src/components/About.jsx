d294ad18998fa7c060e5f74981da73316686ff02
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-02-04 18:14:00 +0500

    added body section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,28 +1,27 @@
-import { Outlet } from "react-router-dom";
-import ProfileFunComponent from "./Profile";
-import ProfileClass from "./ProfileClass";
 import { Component } from "react";
+import food from "../assests/images/food.png";
 
 class About extends Component {
   constructor(props) {
     super(props);
-    console.log("Parent - Constructor");
-  }
-
-  componentDidMount() {
-    // Best place for making API calls
-    console.log("Parent - ComponentDidMount");
   }
 
   render() {
-    console.log("parent - render");
+   
     return (
+      
       <div>
-        <h1>This is about page</h1>
-        <Outlet />  
-        {/* <ProfileClass name={"First child"} /> */}
-        <ProfileFunComponent name={"First child"} />
+        <div className="mt-24 h-[90vh] bg-about_hero bg-no-repeat bg-cover bg-center  flex flex-col justify-center items-center  ">
+          <span className="absolute z-10 w-full min-h-[90vh] bg-black opacity-50"></span>
+          <h1 className="text-white z-30 text-8xl font-bold">foodCaffe'</h1>
+          <p className="text-slate-300 z-30 text-lg font-normal tracking-wider">To eat is a necessity, but to eat intelligently is an art.</p>
+         </div>
+         <div className="py-2 px-44 flex justify-between items-center ">  
+            <h2 className="text-8xl text-[#232946] font-medium leading-[6rem]">Enjoy food all over the world</h2>
+            <img src={food} className="w-1/2 h-96" alt="burger-image" />
+         </div>
       </div>
+
     );
   }
 }
