0756cfa755dc49075cb1f8cc5332eaada029be3e
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-02-04 18:15:19 +0500

    added cart section

--- src/components/Cardt.jsx
+++ src/components/Cardt.jsx
@@ -1,8 +1,11 @@
+import cart_img from '../assests/images/emptycart.png';
 
 const Cart = () => {
     return (
-        <div>
-            <h1>This is cart page</h1>
+        <div className='mt-44 flex flex-col justify-center items-center'>
+            <img src={cart_img} alt="cart-img" />
+            <h1 className='mt-8 text-2xl text-gray-500'>Your cart is empty !</h1>
+            <h4 className='mb-6 text-2xl text-gray-500'>You can go to home page to view more restaurants</h4>
         </div>
     );
 };
