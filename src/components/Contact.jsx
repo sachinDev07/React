b5e06b32a458e2e09105af150a58b5dcf60c01e5
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-02-04 18:15:58 +0500

    added contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,10 @@
+import letter_img from '../assests/images/contact.png';
 
 const Contact = () => {
     return (
-        <div>
-            <h1>This is contact page</h1>
+        <div className='mt-40 flex justify-between items-center px-44 py-2 '>
+            <h1 className='text-6xl text-black font-bold leading-[5rem]'>Have questions ? Shoot us an <span className='text-green-700'>Email</span>.</h1>
+            <img src={letter_img} className="w-1/2" alt="letter-img" />
         </div>
     );
 };
