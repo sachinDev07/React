40793705fe74e27547b05f85b7fcb9a8fbaba1d2
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-02-13 15:21:28 +0500

    updated API

--- src/config.js
+++ src/config.js
@@ -2,10 +2,13 @@ export const IMG_CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 export const FETCH_MENU_URL =
-  "https://www.swiggy.com/dapi/menu/v4/full?lat=19.0759837&lng=72.8776559&menuId=";
+  "https://www.swiggy.com/dapi/menu/v4/full?lat=21.1641592&lng=72.81568109999999&menuId=";
 
 export const API_URL =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&page_type=DESKTOP_WEB_LISTING";
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1641592&lng=72.81568109999999&page_type=DESKTOP_WEB_LISTING";
+
+export const IMG_SLIDER_URL =
+	"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_520,h_520/";
 
 export const restrauntList = [
   {
