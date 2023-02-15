5789fb14c21a0c40072d95b2baec7babe0d56a66
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-02-15 19:40:56 +0500

    fixed issues and added some css

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -15,7 +15,6 @@ const Body = () => {
     allRestaurants, 
     filteredRestaurants, 
     setFilteredRestaurants, 
-    loading,
     sliderRestaurentList
   } = useAllRestaurents();
 
@@ -26,21 +25,21 @@ const Body = () => {
   }
 
 
-  return loading ? (
+  return allRestaurants.length === 0 ? (
     <Shimmer/>
   ) : (
     <>
       <Slider slider={sliderRestaurentList} />
 
       <div className="py-4 px-44 flex justify-center mt-5">
-        <input data-testid="search-input" className="w-[500px] outline-none border-solid border-2 border-gray-600 py-3 px-4 text-xl text-black" 
+        <input data-testid="search-input" className="w-[500px] outline-none border-solid border-2 border-gray-600 py-3 px-4 text-xl text-black rounded-tl rounded-bl" 
           type="text" 
           placeholder="Search your favourite restaurent" 
           value={searchText} 
           onChange={(e) => { setSearchText(e.target.value);}}
         />
 
-        <button data-testid="search-btn" className="px-6 py-1 bg-blue-600 text-white font-medium text-base leading-tight uppercase rounded-r-sm shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" 
+        <button data-testid="search-btn" className="px-6 py-1 bg-blue-600 text-white font-medium text-base leading-tight uppercase rounded-tr rounded-br shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" 
           onClick={() => {
             const data = filterData(searchText, allRestaurants); // need to filter the data
             setFilteredRestaurants(data); // update the state:
