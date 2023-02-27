62d34f5d23011ad90822cba7c25662cc8810c4ef
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-02-27 23:10:04 +0500

    Added component for counting quantity of the items

---
+++ src/components/CountQuantiy.jsx
@@ -0,0 +1,44 @@
+import { useDispatch, useSelector } from "react-redux";
+import { addItem, removeItem } from "../utils/cartSlice";
+
+const CountQuantity = ({ item }) => {
+
+    const cartItems = useSelector(store => store.cart.items);
+
+    const dispatch = useDispatch();
+
+    const addFoodItem = (item) => {
+        dispatch(addItem(item));
+    }
+    
+    const removeFromCart = (item) => {
+        dispatch(removeItem(item));
+    }
+    
+    function getQuantityById(id) {
+        const item = cartItems.find((item) => item.id === id);
+        return item ? item.quantity : null ;
+    }
+
+    return(
+        <div className="flex gap-x-1 items-center text-center rounded">
+            <button 
+                data-testid="addBtn" 
+                className="py-2 px-3 text-sm font-semibold text-white bg-[#0e172c] hover:bg-red-700 transition-all duration-200 ease-linear rounded"
+                onClick={() => removeFromCart(item.id)}
+                ><i className="fa-solid fa-minus"></i>
+            </button>
+
+            <p className="py-[6px] px-3 text-white bg-[#0e172c] rounded"> {getQuantityById(item.id)} </p>
+
+            <button 
+                data-testid="addBtn" 
+                className=" py-2 px-3 text-sm font-semibold  text-white bg-[#0e172c] hover:bg-red-700 transition-all duration-200 ease-linear rounded"
+                onClick={() => addFoodItem(item)}
+            > <i className="fa-solid fa-plus"></i>
+            </button>
+        </div>
+    )
+}
+
+export default CountQuantity;
