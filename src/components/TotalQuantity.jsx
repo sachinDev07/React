297c3511d21ebf3309a8645675f7871eee7ad7bd
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-03-01 22:40:59 +0500

    Added total price functional and some css changes

--- src/components/TotalQuantity.jsx
+++ src/components/TotalQuantity.jsx
@@ -1,27 +1,28 @@
 import emptyCartImage from '.././assests/images/emptycart.png';
-import { useSelector } from 'react-redux';
-import useRestaurentMenu from '../hooks/useRestaurentMenu';
-import { useParams } from 'react-router-dom';
-import CountQuantity from './CountQuantiy';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { addItem, removeItem } from '../utils/cartSlice';
 
 const TotalQuantity = () => {
+
     const cartItems = useSelector(store => store.cart.items);
-
-    const uniqItems = [...new Set(cartItems)];
-
-    console.log("unique",uniqItems);
-
-    const { resId } = useParams();
-
-    const restaurent = useRestaurentMenu(resId);
 
     const dispatch = useDispatch();
 
     const addFoodItem = (item) => {
         dispatch(addItem(item));
     }
+    
+    const removeFromCart = (item) => {
+        dispatch(removeItem(item));
+    }
 
+    const getTotalQuantity = () => {
+        let total = 0
+        cartItems.forEach(item => {
+            total += item.price * item.quantity;
+        })
+        return total/100;
+    }
 
     return (
         <div className='mt-60 flex flex-col'>
@@ -38,39 +39,50 @@ const TotalQuantity = () => {
                     )
                     :
                     (
-                        <div className='max-w-[300px]'>
-                            <h2 className='font-bold text-3xl text-black text-start'>Cart</h2>
-                                <span 
-                                    className='font-semibold text-gray-500 mt-2 text-sm'>{cartItems.length + " ITEM"}
+                        <div className='fixed max-w-[300px] '>
+                                <h2 className='font-bold text-3xl text-black text-start'>Cart</h2>
+                                <span className='font-semibold text-gray-500 mt-2 text-sm'>{cartItems.length + " ITEM"}
                                 </span>
-                            
-                         
+                            <div className='max-h-40 overflow-y-scroll p-2'>
                             {
                                 Object.values(cartItems).map((item) => (
-                                    <div className='flex justify-between items-center mt-6'>
-                                    <span 
-                                        className='font-semibold text-gray-500 mt-2 text-sm'>{}
-                                    </span>
-                                        <div className='w-[155px]'>
+                                    <div key={item.id} className='flex justify-between items-center mt-4 '>
+                                        <div className='w-[155px] mr-2'>
                                             {item.name}
                                         </div>
-                                        <div 
-                                            className='py-1 px-3 flex gap-x-3 border-2 border-green-300 border-solid max-w-[90px]'>
-                                            <button>
+                                        <div className='py-1 px-3 flex gap-x-3 border-2 border-green-300 border-solid max-w-[90px] mr-8'>
+                                            <button onClick={() => removeFromCart(item?.id)}>
                                                 <i className="fa-solid fa-minus"></i>
                                             </button>
+                                            
                                             <p>{item.quantity}</p>
-                                            <button>
+
+                                            <button onClick={() => addFoodItem(item)}>
                                                 <i className="fa-solid fa-plus"></i>
                                             </button>
                                         </div>
                                         <div className='font-semibold'>
                                             {item.price / 100}
-                                        </div>
-                                    </div>
+                                        </div> 
+                                    </div>   
                                 ))
+                                  
                             }
+                            </div>
+                            <div className='mt-5 flex justify-between'>
+                                <div>
+                                    <div className='text-lg font-bold text-gray-800'>Sub Total</div>
+                                    <div className='text-xs text-gray-400'>Extra charges may apply</div>
+                                </div>
+                                 <div className='text-lg font-bold text-gray-800'>Rs. {getTotalQuantity()}</div>
+                            </div>
+                            <button 
+                                type='button' 
+                                className='w-full rounded mt-4 py-2 text-white font-bold text-lg bg-green-600 hover:bg-green-500 transition duration-200 ease-linear'>
+                                CHECKOUT  
+                            </button>
                         </div>
+                        
                     )
                 }
                 
